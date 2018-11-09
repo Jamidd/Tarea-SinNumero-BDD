40020c404408203147db937500667170fbe8c7cf
Jamidd <jamiddleton@uc.cl>
2018-11-09 19:18:44 -0300

    mensajesok

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import json
 from pprint import pprint
 
-with open('usuarios.json') as f:
-    data = json.load(f)
+with open('messages.json', "r") as f:
+	aux = f.readlines()
+	print(aux.pop(0).strip())
+	id = 0
+	while "]" not in aux[0]:
+		print(aux.pop(0).strip())
+		print('"id": {},'.format(id))
+		id += 1
+		print(aux.pop(0).strip())
+		print(aux.pop(0).strip())
+		print(aux.pop(0).strip())
+		print(aux.pop(0).strip())
+		print(aux.pop(0).strip())
+		print(aux.pop(0).strip())
+		print(aux.pop(0).strip())
 
-pprint(data)
+	print(aux.pop(0).strip())
+
+		
+
+
 
