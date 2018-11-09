8a85f9994fb322bd98bd9d640d9577d2cbef082e
IchottMano <reschilling@uc.cl>
2018-11-09 20:23:37 -0300

    untested text search complete

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Grupo 23
 
-Para importar las bases de datos, se debe correr en la consola de mongo:
+Para importar las bases de datos, se debe correr en la consola:
 
 ```bash
 mongoimport --db test --collection usuarios --drop --file <PATH archivo usuarios.json> --jsonArray
@@ -15,6 +15,12 @@ mongoimport --db test --collection messages --drop --file <PATH archivo messages
 Por favor notar que <PATH archivo *.json> son paths que tienen que ser agregados por el ayudante.
 
 Con esto se suben las bases de datos a MongoDB
+
+Para que nuestra búsqueda de texto funcione correctamente, ahora debemos entrar a la consola de mongo y correr el comando:
+
+```python
+db.messages.createIndex( { message: "text" } );
+```
 
 Para correr la API se debe correr el comando:
 
@@ -33,6 +39,25 @@ Por lo tanto, debe entrar a la página [http://localhost:5000/](http://localhost
 (GET) Para ver la información de un mensaje, se ingresa a la ruta: /message/\<id\>
 
  - El ID de un mensaje fue agregado manualmente, permitido por [esta issue](https://github.com/IIC2413/Syllabus-2018-2/issues/147). El output es la información del mensaje.
+
+(GET) Para ver los mensajes entre dos usuarios, se ingresa a la ruta /users/\<id1\>&\<id2>
+
+- Es importante que la separación entre los ID sea "&". El output es una lista de los mensajes que fueron intercambiados.
+
+(GET) Para buscar texto, se debe ingresar a la ruta: /search y anotar los parámetros
+
+- user=\<id\> para buscar un id de usuario determinado
+- 1=\<texto\> para agregar frases que si o si deben estar en el mensaje, las frases deben estar separadas por el caracter "|"
+- 2=\<texto\> para agregar palabras que deseablemente deben estar, separadas por "|"
+- 3=\<texto\> para agregar palabras que estan prohibidas, separadas por "|"
+
+Estos parámetros deben tener antepuestos el símbolo "?" y para separarlos se debe hacer con el símbolo "&". Por ejemplo:
+
+```
+localhost:5000/search?id=1&1=hola|como|estas&2=uwu|owo&3=fernando|pieressa
+```
+
+La ruta anterior busca los mensajes del usuario con id=1, que tenga las palabras "hola", "como", "estas" y que tenga "owo" o "uwu" en la frase, mientras que no puede tener las palabras "fernando" o "pieressa".
 
 
 
--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, abort, request
 from pymongo import MongoClient
 import sys
 
-
 app = Flask(__name__)
 
 PORT = 5000
@@ -21,7 +20,7 @@ def remove_id(obj):
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return "Hello World!", 200
 
 
 @app.route('/user/<int:user>', methods=['GET'])
@@ -35,7 +34,7 @@ def get_user(user):
     for s in users.find({"id": user}):
         user_output.append(s)
     if len(user_output) == 0:
-        return jsonify(), 404
+        return "El usuario con ID = {} no existe.".format(user), 404
     else:
         remove_id(user_output)
         output.update({"user": user_output[0]})
@@ -54,10 +53,76 @@ def get_message(message):
     for s in messages.find({"id": message}):
         output.append(s)
     if len(output) == 0:
-        return jsonify(), 404
+        return "El mensaje con ID = {} no existe.".format(message), 404
     else:
         remove_id(output)
         return jsonify(output[0]), 200
+
+
+@app.route('/users/<users>', methods=['GET'])
+def get_inter(users):
+    user_list = users.split('&')
+    if len(user_list) != 2:
+        return "Formato inválido, por favor intente nuevamente", 400
+    mongodb = client[MONGODATABASE]
+    messages = mongodb.messages
+    output = list()
+    for s in messages.find(
+            {"$or": [{"$and": [{"sender": int(user_list[0]),
+                                "receptant": int(user_list[1])}]},
+                     {"$and": [{"sender": int(user_list[1]),
+                                "receptant": int(user_list[0])}]}]}):
+        output.append(s)
+    if len(output) == 0:
+        return "No se encontraron mensajes", 404
+    else:
+        remove_id(output)
+        return jsonify(output), 200
+
+
+@app.route('/search')
+def search_text():
+    user = request.args.get('user', None)
+    mandatory = request.args.get('1', None)
+    non_mand = request.args.get('2', None)
+    forbidden = request.args.get('3', None)
+    print(user, mandatory, non_mand, forbidden)
+    queries = list()
+    if user is not None:
+        queries.append({"sender": int(user)})
+    if non_mand is not None:
+        non_mand = non_mand.split('|')
+        non_words = list()
+        for word in non_mand:
+            non_words.append({"$text": {"$search": word}})
+        queries.append({"$or": non_words})
+    if mandatory is not None:
+        mandatory = mandatory.split('|')
+        words = list()
+        for frase in mandatory:
+            words.append("\"" + frase + "\"")
+        queries.append({"$text": {"$search": ' '.join(words)}})
+    if forbidden is not None:
+        for_words = forbidden.split('|')
+        new_words = list()
+        for word in for_words:
+            new_words.append("-" + word)
+        nowo = ' '.join(new_words)
+        print(nowo, "nowo")
+        queries.append({"$text": {"$search": nowo}})
+    if not queries:
+        return "No se hicieron queries", 400
+    else:
+        mongodb = client[MONGODATABASE]
+        messages = mongodb.messages
+        output = list()
+        for s in messages.find({"$and": queries}):
+            output.append(s)
+        if len(output) == 0:
+            return "No se encontraron mensajes", 404
+        else:
+            remove_id(output)
+            return jsonify(output), 200
 
 
 if __name__ == '__main__':
